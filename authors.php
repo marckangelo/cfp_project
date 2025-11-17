90d99ce448d6b13fe0c8c4e3dd4966c727ea0e76
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-17 15:44:35 -0500

    Completed author.php
    Fetch and display authors from the database with links to their items.

--- authors.php
+++ authors.php
@@ -2,7 +2,21 @@
 session_start();
 require 'db.php';
 include 'header.php';
-?>
-<h2>Authors</h2>
-<p>TODO: List authors and link to their items.</p>
+
+
+$query = "SELECT id, name, orchid FROM authors";
+$result = mysqli_query($conn, $query);
+
+echo "<h2>Authors</h2>";
+echo "<ul>";
+while ($author = mysqli_fetch_assoc($result)) {
+  echo "<li>
+    <a href='author.php?id=" . $author['id'] . "'>" . 
+    htmlspecialchars($author['name']) . 
+    "</a> (ORCHID: " . 
+    htmlspecialchars($author['orchid'] / ")
+    </li>";
+}
+echo "</ul>";
+
 <?php include 'footer.php'; ?>
