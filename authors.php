a04bd66ab597d166838cb239c8e216a334e18ddf
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-17 16:03:11 -0500

    Fix SQL query and loop variable in authors.php
    Updated SQL query to select 'orcid' instead of 'orchid' and fixed the loop variable from '$author' to '$row'.

--- authors.php
+++ authors.php
@@ -3,18 +3,19 @@ session_start();
 require 'db.php';
 include 'header.php';
 
-
-$query = "SELECT id, name, orchid FROM authors";
+//gets the data from the sql table AUTHOR
+$query = "SELECT id, name, orcid FROM authors";
 $result = mysqli_query($conn, $query);
 
 echo "<h2>Authors</h2>";
+//displays unorded lists
 echo "<ul>";
-while ($author = mysqli_fetch_assoc($result)) {
-  echo "<li>
-    <a href='author.php?id=" . $author['id'] . "'>" . 
-    htmlspecialchars($author['name']) . 
-    "</a> (ORCHID: " . 
-    htmlspecialchars($author['orchid'] / ")
+while ($row = mysqli_fetch_assoc($result)) {
+  echo "<li>;
+  <a href='author.php?id=" . $author['id'] . "'>" . 
+    htmlspecialchars($row['name']) . 
+    "</a> (ORCID: " . 
+    htmlspecialchars($row['orcid'] / ")
     </li>";
 }
 echo "</ul>";
