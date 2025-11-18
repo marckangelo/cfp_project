bd32347085e0e967617bb52a23fb7715e6c7be08
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-18 00:49:02 -0500

    started committee_request.php and almost completed donate.ph

--- committee_request.php
+++ committee_request.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 require 'db.php';
-
+if ($_SESSION['member_id']) {
+    echo "Only members may join committees.";
+}
 // TODO: Insert committee join request for logged-in member
+
+
 ?>
