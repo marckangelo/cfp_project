d18acc78b513c80ce9e5bd8a3f940ee40f7bb2f9
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-17 23:32:22 -0500

    Update committees.php
    fixed syntax issue

--- committees.php
+++ committees.php
@@ -6,7 +6,7 @@ include 'header.php';
 
 echo "<h2>Committees</h2>";
 //retreiving data
-$query = "SELECT id, name, description FROM committee";
+$query = "SELECT committee_id, name, description FROM committee";
 $commity = mysqli_query($conn, $query);
 
 //checks if there are any committees present 
