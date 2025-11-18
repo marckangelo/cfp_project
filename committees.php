15160a0cb76a8fb6c5c509f23dc7b13067368218
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-17 22:45:41 -0500

    Update committees.php
    added comments

--- committees.php
+++ committees.php
@@ -5,20 +5,24 @@ include 'header.php';
 
 
 echo "<h2>Committees</h2>";
+//retreiving data
 $query = "SELECT id, name, description FROM committees";
 $commity = mysqli_query($conn, $query);
 
+//checks if there are any committees present 
 if (mysqli_num_rows($commity) > 0) {
     echo "<ul>";
     while ($row = mysqli_fetch_assoc($commity)) {
         echo "<li>" . htmlspecialchars($row['name']);
 
         echo htmlspecialchars($row['description']);
-
+        //checks if a user is logged in 
         if ($_SESSION['member_id']) {
             echo " < form style='display:inline' method='POST
             action = 'request_comittee.php'>
+            //silently sends the database the id of the committee
             <input type= 'hidden' name='committee_id' value= ' " . $row['id'] . "'>
+            //displays a join button
             <button type='submit'>JOIN</button>
             </form>";
         }
@@ -26,6 +30,7 @@ if (mysqli_num_rows($commity) > 0) {
     }
     echo "</lu>";
 }
+//displays message 
 else {
     echo "<p> No committees available. </p>";
 }
