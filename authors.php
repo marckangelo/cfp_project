37aee776dbc22bce209d47c2f6b5826caad7127e
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-17 23:39:58 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -4,7 +4,7 @@ require 'db.php';
 include 'header.php';
 
 //gets the data from the sql table AUTHOR
-$query = "SELECT member_id, name, orcid FROM author";
+$query = "SELECT member_id, bio, orcid FROM author";
 $result = mysqli_query($conn, $query);
 
 echo "<h2>Authors</h2>";
@@ -13,7 +13,7 @@ echo "<ul>";
 while ($row = mysqli_fetch_assoc($result)) {
   echo "<li>;
   <a href='author.php?id=" . $author['id'] . "'>" . 
-    htmlspecialchars($row['name']) . 
+    htmlspecialchars($row['bio']) . 
     "</a> (ORCID: " . 
     htmlspecialchars($row['orcid']) / ")
     </li>";
