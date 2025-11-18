edc6c22a500922b622657c11a458395cd5ad7d99
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-17 22:35:32 -0500

    Update committees.php

--- committees.php
+++ committees.php
@@ -3,8 +3,31 @@ session_start();
 require 'db.php';
 include 'header.php';
 
-// TODO: List committees and allow logged-in members to request to join
-?>
-<h2>Committees</h2>
-<p>TODO: Show committees and 'Request to join' buttons.</p>
-<?php include 'footer.php'; ?>
+
+echo "<h2>Committees</h2>";
+$query = "SELECT id, name, description FROM committees";
+$commity = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($commity) > 0) {
+    echo "<ul>";
+    while ($row = mysqli_fetch_assoc($commity)) {
+        echo "<li>" . htmlspecialchars($row['name']);
+
+        echo htmlspecialchars($row['description']);
+
+        if ($_SESSION['member_id']) {
+            echo " < form style='display:inline' method='POST
+            action = 'request_comittee.php'>
+            <input type= 'hidden' name='committee_id' value= ' " . $row['id'] . "'>
+            <button type='submit'>JOIN</button>
+            </form>";
+        }
+        echo "</li>";
+    }
+    echo "</lu>";
+}
+else {
+    echo "<p> No committees available. </p>";
+}
+
+include 'footer.php'; ?>
