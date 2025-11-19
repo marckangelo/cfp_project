3560d151bfe48589c9803abcea964e2a7f9954b9
Marck Angelo Geli <marckangelogeli@yahoo.com>
2025-11-19 00:28:25 -0500

    Fix list of authors in authors.php page. Also, added clickable links to add or edit an item in the authors_dashboard.php

--- author_dashboard.php
+++ author_dashboard.php
@@ -7,4 +7,10 @@ include 'header.php';
 ?>
 <h2>Author Dashboard</h2>
 <p>TODO: Show author's items and links to create/edit items.</p>
+
+<ul>
+    <li><a href="author_item_new.php">Add a new item</a></li>
+    <li><a href="author_item_edit.php">Edit an item</a></li>
+</ul>
+
 <?php include 'footer.php'; ?>
--- authors.php
+++ authors.php
@@ -3,21 +3,59 @@ session_start();
 require 'db.php';
 include 'header.php';
 
-//gets the data from the sql table AUTHOR
-$query = "SELECT member_id, bio, orcid FROM author";
-$result = mysqli_query($conn, $query);
+// TODO: Admin-only member management (view, change status, etc.)
 
-echo "<h2>Authors</h2>";
-//displays unorded lists
-echo "<ul>";
-while ($row = mysqli_fetch_assoc($result)) {
-  echo "<li>;
-  <a href='author.php?id=" . $author['id'] . "'>" . 
-    htmlspecialchars($row['bio']) . 
-    "</a> (ORCID: " . 
-    htmlspecialchars($row['orcid']) / ")
-    </li>";
+// Checking if signed in as member **** SHOULD BE CHECKING IF SIGNED IN AS ADMIN ****
+if (isset($_SESSION['member_id'])) {
+    // DISPLAY MEMBER DETAILS
+    $sql_author_details = "SELECT 
+                          a.orcid,
+                          m.name,
+                          m.organization,
+                          m.primary_email,
+                          a.specialization,
+                          a.h_index,
+                          a.total_downloads
+                          FROM author a
+                          JOIN member m ON a.member_id = m.member_id;
+                          ";
+    
+    // Run the query
+    $result_author_details = mysqli_query($conn, $sql_author_details);
+
+    // Table header
+    echo '
+    <h4>Member Details</h4>
+
+        <table border="1">
+            <tr>
+                <th>Orcid</th>
+                <th>Name</th>
+                <th>Organization</th>
+                <th>Primary Email</th>
+                <th>Specialization</th>
+                <th>h index</th>
+                <th>Total Downloads</th>
+            </tr>
+    ';
+    
+    //Table rows (Fetching each row from member detail using while loop)
+    while ($row = mysqli_fetch_assoc($result_author_details)) {
+        echo '
+            <tr>
+                <td>' . htmlspecialchars($row['orcid']) . '</td>
+                <td>' . htmlspecialchars($row['name']) . '</td>
+                <td>' . htmlspecialchars($row['organization']) . '</td>
+                <td>' . htmlspecialchars($row['primary_email']) . '</td>
+                <td>' . htmlspecialchars($row['specialization']) . '</td>    
+                <td>' . htmlspecialchars($row['h_index']) . '</td>
+                <td>' . htmlspecialchars($row['total_downloads']) . '</td>
+            </tr>';
+    }
+    echo '</table>';
 }
-echo "</ul>";
 
-include 'footer.php'; ?>
+?>
+<h2>Authors</h2>
+<p>TODO: List authors.</p>
+<?php include 'footer.php'; ?>
