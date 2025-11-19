1971aa03e2edda8eadcebe6eaf9ff6be7083b7ab
Marck Angelo Geli <marckangelogeli@yahoo.com>
2025-11-19 18:08:16 -0500

    Added display of donations in my_account.php page.

--- my_account.php
+++ my_account.php
@@ -132,10 +132,97 @@ if (isset($_SESSION['member_id'])) {
     echo '</table>';
 
 
+    // ============= DISPLAY LIST OF DONATIONS ================
+
+    // DISPLAY DONATION DETAILS
+    $member_id = $_SESSION['member_id'];
+
+    $sql_donation_details = "SELECT d.date,
+                                    d.amount,
+                                    d.currency,
+                                    d.payment_method,
+                                    d.transaction_id,
+                                    d.charity_pct,
+                                    d.cfp_pct,
+                                    d.author_pct,
+                                    
+                                    t.title AS text_title,
+                                    t.topic AS text_topic,
+                                    t.version AS text_version,
+                                    
+                                    c.name AS charity_name,
+                                    c.country AS charity_country,
+                                    c.description AS charity_description,
+                                    c.mission AS charity_mission
+                             FROM donation d
+                             JOIN text t
+                                 ON d.text_id = t.text_id
+                             JOIN charity c
+                                 ON d.charity_id = c.charity_id
+                             WHERE d.member_id = $member_id
+                             ORDER BY d.date DESC";
+
+    
+    // Run the query
+    $result_donation_details = mysqli_query($conn, $sql_donation_details);
+
+    // Table header
+    echo '
+    <h4>Donation Details</h4>
+
+        <table border="1">
+            <tr>
+                <th>Donation Date</th>
+                <th>Amount</th>
+                <th>Currency</th>
+                <th>Payment Method</th>
+                <th>TransactionID</th>
+                <th>Charity (%)</th>
+                <th>CFP (%)</th>
+                <th>Author (%)</th>
+                
+                <th>Title</th>
+                <th>Topic</th>
+                <th>Text Version</th>
+
+                <th>Charity Name</th>
+                <th>Charity Country</th>
+                <th>Charity Description</th>
+                <th>Charity Mission</th>
+            </tr>
+    ';
+    
+    //Table rows (Fetching each row from member detail using while loop)
+    while ($row_donation = mysqli_fetch_assoc($result_donation_details)) {
+        echo '
+            <tr>
+                <td>' . htmlspecialchars($row_donation['date']) . '</td>
+                <td>' . htmlspecialchars($row_donation['amount']) . '</td>
+                <td>' . htmlspecialchars($row_donation['currency']) . '</td>
+                <td>' . htmlspecialchars($row_donation['payment_method']) . '</td>
+                <td>' . htmlspecialchars($row_donation['transaction_id']) . '</td>
+                <td>' . htmlspecialchars($row_donation['charity_pct']) . '</td>
+                <td>' . htmlspecialchars($row_donation['cfp_pct']) . '</td>
+                <td>' . htmlspecialchars($row_donation['author_pct']) . '</td>
+                
+                <td>' . htmlspecialchars($row_donation['text_title']) . '</td>
+                <td>' . htmlspecialchars($row_donation['text_topic']) . '</td>
+                <td>' . htmlspecialchars($row_donation['text_version']) . '</td>
+                
+                <td>' . htmlspecialchars($row_donation['charity_name']) . '</td>
+                <td>' . htmlspecialchars($row_donation['charity_country']) . '</td>
+                <td>' . htmlspecialchars($row_donation['charity_description']) . '</td>
+                <td>' . htmlspecialchars($row_donation['charity_mission']) . '</td>
+            </tr>';
+    }
+    echo '</table>';
+
 } else {
     header("Location: login.php");
     exit;
 }
+
+
 
 ?>
 <h2>My Account</h2>
