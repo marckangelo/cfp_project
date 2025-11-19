733be7528fa9701a860c68c7b491d6ea4487f27c
Marck Angelo Geli <marckangelogeli@yahoo.com>
2025-11-19 01:19:40 -0500

    Added the check to see if the current user is signed in as a member to access the item.php page.

--- download.php
+++ download.php
@@ -3,4 +3,12 @@ session_start();
 require 'db.php';
 
 // TODO: Check login, enforce download rules, log the download and serve the file
+
+if (isset($_SESSION['member_id'])) {
+
+    
+
+} else {
+    header("Location: login.php");
+}
 ?>
