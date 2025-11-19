d5e5dd27866819472c323462febb9ef28c960ad8
Marck Angelo Geli <marckangelogeli@yahoo.com>
2025-11-19 13:59:38 -0500

    Added required to be a member to be able to  process donate page.

--- donate_process.php
+++ donate_process.php
@@ -3,4 +3,14 @@ session_start();
 require 'db.php';
 
 // TODO: Process donation form, insert into database, enforce allocation rules
+
+if (isset($_SESSION['member_id'])) {
+
+// CODE HERE
+
+
+} else {
+    header("Location: login.php");
+    exit;
+}
 ?>
