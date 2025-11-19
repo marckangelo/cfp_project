032d5a3a3d3ad35c0b9f7de7b649f4faa8bebf88
Marck Angelo Geli <marckangelogeli@yahoo.com>
2025-11-19 02:40:55 -0500

    Added download functionality of items (texts) in item.php. Also added the list of items (texts) downloaded in the my_account.php page. Currently allowed to download the same item multiple times.

--- download.php
+++ download.php
@@ -6,7 +6,29 @@ require 'db.php';
 
 if (isset($_SESSION['member_id'])) {
 
-    
+    // Retrieve data to insert into download table
+    $member_id = $_SESSION['member_id'];
+    $text_id = $_POST['text_id'];
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+    // Get country data from member table
+    $sql_country = "SELECT country FROM member WHERE member_id = $member_id";
+    $result_country = mysqli_query($conn, $sql_country);
+    $country_row = mysqli_fetch_assoc($result_country);
+    $country = $country_row['country'];
+
+    $sql_download = "INSERT INTO download (member_id, text_id, download_date, ip_address, user_agent, country)
+                    VALUES ($member_id, $text_id, NOW(), '$ip_address', '$user_agent', '$country')";
+
+    $result_download = mysqli_query($conn, $sql_download);
+
+    if ($result_download) {
+        $_SESSION['download_success'] = "Text Successfully Downloaded!";
+    }
+
+    // Head to the my_account.php page after successful download to see list of downloads
+    header("Location: my_account.php");
 
 } else {
     header("Location: login.php");
--- my_account.php
+++ my_account.php
@@ -7,7 +7,7 @@ include 'header.php';
 
 // Checking if signed in
 if (isset($_SESSION['member_id'])) {
-    // DISPLAY MEMBER DETAILS
+    // ================ DISPLAY MEMBER DETAILS =================
     $sql_member_details = "SELECT * 
                            FROM member
                            WHERE member_id = " . $_SESSION['member_id'];
@@ -59,6 +59,66 @@ if (isset($_SESSION['member_id'])) {
             <button type="submit" name="edit_profile">Edit</button>
         </form>
     ';
+
+
+
+    // ============= DISPLAY LIST OF DOWNLOADS ================
+
+    // DISPLAY MEMBER DETAILS
+    $member_id = $_SESSION['member_id'];
+
+    $sql_download_details = "SELECT 
+                                d.download_date,
+                                t.text_id,
+                                t.title,
+                                t.topic,
+                                t.version,
+                                t.status,
+                                a.orcid AS author_orcid,
+                                m.name AS author_name
+                             FROM download d
+                             JOIN text t
+                                ON d.text_id = t.text_id
+                             JOIN author a
+                                ON t.author_orcid = a.orcid
+                             JOIN member m
+                                ON a.member_id = m.member_id
+                             WHERE d.member_id = $member_id
+                             ORDER BY d.download_date DESC";
+    
+    // Run the query
+    $result_download_details = mysqli_query($conn, $sql_download_details);
+
+    // Table header
+    echo '
+    <h4>Download Details</h4>
+
+        <table border="1">
+            <tr>
+                <th>Download Date</th>
+                <th>Title</th>
+                <th>Topic</th>
+                <th>Version</th>
+                <th>Status</th>
+                <th>Author</th>
+            </tr>
+    ';
+    
+    //Table rows (Fetching each row from member detail using while loop)
+    while ($row_download = mysqli_fetch_assoc($result_download_details)) {
+        echo '
+            <tr>
+                <td>' . htmlspecialchars($row_download['download_date']) . '</td>
+                <td>' . htmlspecialchars($row_download['title']) . '</td>
+                <td>' . htmlspecialchars($row_download['topic']) . '</td>
+                <td>' . htmlspecialchars($row_download['version']) . '</td>
+                <td>' . htmlspecialchars($row_download['status']) . '</td>
+                <td>' . htmlspecialchars($row_download['author_name']) . '</td>
+                <td><button>Change Status<button></th>
+            </tr>';
+    }
+    echo '</table>';
+
 
 } else {
     header("Location: login.php");
