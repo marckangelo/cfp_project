47a0ae83f687b35f33a816780ba22acc92ab4ab1
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-20 22:59:17 -0500

    Update comment_add.php

--- comment_add.php
+++ comment_add.php
@@ -3,14 +3,7 @@ session_start();
 require 'db.php';
 
 // TODO: Insert a new comment for an item by the logged-in member
-/*
-Each contributing member(author) has a home page which has an index of his/her contents. The
-number of downloads, the to-date-contributions made for the title. List of other text that were
-derived new versions of each of the authors text.. Any CFP members who had downloaded a title
-by the author could add comments about the title. The author could respond to such comments
-and may update, errors etc, in the title if required. Such updated version is checked by a CFP
-moderator and would replace the original text.
-*/
+
 if (!isset($_SESSION['member_id'])) {
     header("Location: login.php");
     exit;
@@ -26,7 +19,7 @@ if ($downloads_by_member_result) {
     while ($row = mysqli_fetch_assoc($downloads_by_member_result)) {
         $downloads_by_member[] = $row;
     }
-} 
+}
 // Process form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $text_id = $_POST['text_id'];
@@ -36,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $is_public = isset($_POST['is_public']) ? 1 : 0;
 
     // Validate comment text
-    if (empty($$content)) {
+    if (empty($content)) {
         $errors[] = "Comment text cannot be empty.";
     }
 
@@ -51,25 +44,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             break;
         }
     }
+    // If not downloaded, add error
     if (!$has_downloaded) {
         $errors[] = "You must download the text before commenting.";
     }
     // If no errors, insert the comment
     if (empty($errors)) {
-        $sql_insert_comment = "INSERT INTO comments (member_id, text_id, content, date, rating, is_public)
-                               VALUES ($member_id, $text_id, ?, '$date', $rating, $is_public)";
-        $stmt = mysqli_prepare($conn, $sql_insert_comment);
-        mysqli_stmt_bind_param($stmt, 's', $content);
-        $result_insert = mysqli_stmt_execute($stmt);
+        $comment_added = htmlspecialchars($content);
 
-        if ($result_insert) {
+        $sql_insert_comment = "INSERT INTO comment (member_id, text_id, content, date, rating, is_public)
+                               VALUES ($member_id, $text_id, '$comment_added', '$date', $rating, $is_public)";
+                        
+        $result_insert_comment = mysqli_query($conn, $sql_insert_comment);
+        // Redirect back to item page after successful comment
+        if ($result_insert_comment) {
             header("Location: item.php?text_id=$text_id");
             exit;
+        // display error if insertion fails
         } else {
             $errors[] = "Failed to add comment. Please try again.";
         }
     }
-    
-}
 
+}
 ?>
