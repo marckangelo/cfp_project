268d83397fe3d445795d1db7e7a07a7aecfeabc1
Arshdeep3113 <arshdeep200423@gmail.com>
2025-11-26 01:07:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ if (isset($_GET['search'])) {
 $query_search = "SELECT DISTINCT t.topic, k.keyword, m.name FROM text t 
 JOIN author a ON t.author_orcid = a.orcid
 JOIN member m ON a.member_id = m.member_id
-LEFT JOIN text_keyword k ON k.text_id = t.text_id";
+LEFT JOIN text_keyword k ON k.text_id = t.text_id
+WHERE 1=1";
 
 if (!empty($search)) {
     $search_escaped = mysqli_real_escape_string($conn, $search);
