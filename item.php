92a87540bab906cdd27c20d6caeac2ad1ec4d2d3
Marck Angelo Geli <marckangelogeli@yahoo.com>
2025-12-02 12:42:43 -0500

    Non-logged in members are now able to view the text details on item.php page.
    Also added some comments and fixed some indentations

--- item.php
+++ item.php
@@ -56,11 +56,13 @@ $sql_text_details = "SELECT * FROM text WHERE text_id = $text_id";
 
 $result_text_details = mysqli_query($conn, $sql_text_details);
 
+// Default values for plagiarism committee flags (for guests or non-committee members)
+$is_plag_committee_member = false;
+$committee_id_for_user    = null;
+
 if(isset($_SESSION['member_id'])) {
 
     // Check if the logged-in member is part of an active plagiarism committee
-    $is_plag_committee_member = false;
-    $committee_id_for_user    = null;
 
     $member_id = (int) $_SESSION['member_id'];
 
@@ -83,197 +85,194 @@ if(isset($_SESSION['member_id'])) {
         $is_plag_committee_member = true;
         $committee_id_for_user = (int)$row_plag['committee_id'];
     }
+}
 
-    if ($result_text_details) {
-        if (mysqli_num_rows($result_text_details) > 0) {
+if ($result_text_details) {
+    if (mysqli_num_rows($result_text_details) > 0) {
 
-            // Table header
+        // Table header
+        echo '
+        <h2>Item Details</h2>
+
+            <table border="1">
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Abstract</th>
+                    <th>Topic</th>
+                    <th>Keywords</th>
+                    <th>Version</th>
+                    <th>Upload Date</th>
+                    <th>Status</th>
+                    <th>Download Count</th>
+                    <th>Total Donations ($)</th>
+                    <th>Average Rating</th>
+                    <th>Comments</th>
+                    <th>Action</th>
+                </tr>
+        ';
+
+        // Table rows
+        while ($row = mysqli_fetch_assoc($result_text_details)) {
+
+            // Fetch keywords for this text
+            $keywords = array();
+
+            $text_id = (int)$row['text_id'];
+
+            // Fetch author name
+            $author_name = "-";
+            if (!empty($row['author_orcid'])) {
+
+                $author_orcid_sql = mysqli_real_escape_string($conn, $row['author_orcid']);
+
+                $sql_author_name = "
+                    SELECT m.name
+                    FROM author a
+                    JOIN member m
+                        ON a.member_id = m.member_id
+                    WHERE a.orcid = '$author_orcid_sql'
+                    LIMIT 1
+                ";
+
+                $result_author_name = mysqli_query($conn, $sql_author_name);
+
+                if ($result_author_name && mysqli_num_rows($result_author_name) > 0) {
+                    $row_author_name = mysqli_fetch_assoc($result_author_name);
+                    $author_name = $row_author_name['name'];
+                }
+            }
+
+
+            // Retrieving all keywords for this text
+            $sql_text_keywords = "SELECT keyword 
+                                FROM text_keyword 
+                                WHERE text_id = $text_id";
+
+            $result_text_keywords = mysqli_query($conn, $sql_text_keywords);
+
+            while ($row_kw = mysqli_fetch_assoc($result_text_keywords)) {
+                $keywords[] = $row_kw['keyword'];
+            }
+
+            $keywords_string = implode(", ", $keywords);
+
+
+            // Retrieving all comments for this text (shows name of member and their comment)
+            $comments = array();
+            $comment_ids = array();
+
+            $sql_text_comments = "SELECT comment_id, member_id, content 
+                                  FROM comment 
+                                  WHERE text_id = $text_id";
+            $result_text_comments = mysqli_query($conn, $sql_text_comments);
+
+            while ($row_comments = mysqli_fetch_assoc($result_text_comments)) {
+
+                $comment = $row_comments['content'];
+
+                // Get member name
+                $sql_member_name = "SELECT name 
+                                    FROM member
+                                    WHERE member_id = " . (int)$row_comments['member_id'];
+                $result_member_name = mysqli_query($conn, $sql_member_name);
+                $row_member_name = mysqli_fetch_assoc($result_member_name);
+                $member_name = $row_member_name['name'];
+
+                // Save raw text. Escape + add the <br> later
+                $comments[]    = $comment . "\n\n" . "Posted by [$member_name]";
+                $comment_ids[] = (int)$row_comments['comment_id'];
+            }
+
+            $keywords_string = implode(", ", $keywords);
+
+            // *** DELETE THE BUTTON DOESN'T DO ANYTHING YET. IT'S JUST THERE  FOR NOW***
             echo '
-            <h2>Item Details</h2>
+            
+                <tr>
+                    <td>' . htmlspecialchars($row['title']) . '</td>
+                    <td>' . htmlspecialchars($author_name) . '</td>
+                    <td>' . htmlspecialchars($row['abstract']) . '</td>
+                    <td>' . htmlspecialchars($row['topic']) . '</td>
+                    <td>' . htmlspecialchars($keywords_string) . '</td>
+                    <td>' . htmlspecialchars($row['version']) . '</td>
+                    <td>' . htmlspecialchars($row['upload_date']) . '</td>
+                    <td>' . htmlspecialchars($row['status']) . '</td> 
+                    <td>' . htmlspecialchars($row['download_count']) . '</td> 
+                    <td>' . htmlspecialchars($row['total_donations']) . '</td>
+                    <td>' . htmlspecialchars($row['avg_rating']) . '</td>
+                    
+                    <td>';
 
-                <table border="1">
-                    <tr>
-                        <th>Title</th>
-                        <th>Author</th>
-                        <th>Abstract</th>
-                        <th>Topic</th>
-                        <th>Keywords</th>
-                        <th>Version</th>
-                        <th>Upload Date</th>
-                        <th>Status</th>
-                        <th>Download Count</th>
-                        <th>Total Donations ($)</th>
-                        <th>Average Rating</th>
-                        <th>Comments</th>
-                        <th>Action</th>
-                    </tr>
-            ';
+                    // Display the comments + reply button (only for the author of this text)
+                    for ($i = 0; $i < count($comments); $i++) {
 
-            // Table rows
-            while ($row = mysqli_fetch_assoc($result_text_details)) {
+                        // Show comment text with line breaks
+                        echo nl2br(htmlspecialchars($comments[$i])) . '<br>';
 
-                // Fetch keywords for this text
-                $keywords = array();
+                        // If author is logged in AND they are the author of this text, show Reply button
+                        if (isset($_SESSION['orcid']) &&
+                            isset($row['author_orcid']) &&
+                            $_SESSION['orcid'] === $row['author_orcid']) {
 
-                $text_id = (int)$row['text_id'];
-
-                // Fetch author name
-                $author_name = "-";
-                if (!empty($row['author_orcid'])) {
-
-                    $author_orcid_sql = mysqli_real_escape_string($conn, $row['author_orcid']);
-
-                    $sql_author_name = "
-                        SELECT m.name
-                        FROM author a
-                        JOIN member m
-                            ON a.member_id = m.member_id
-                        WHERE a.orcid = '$author_orcid_sql'
-                        LIMIT 1
-                    ";
-
-                    $result_author_name = mysqli_query($conn, $sql_author_name);
-
-                    if ($result_author_name && mysqli_num_rows($result_author_name) > 0) {
-                        $row_author_name = mysqli_fetch_assoc($result_author_name);
-                        $author_name = $row_author_name['name'];
-                    }
-                }
-
-
-                // Retrieving all keywords for this text
-                $sql_text_keywords = "SELECT keyword 
-                                    FROM text_keyword 
-                                    WHERE text_id = $text_id";
-
-                $result_text_keywords = mysqli_query($conn, $sql_text_keywords);
-
-                while ($row_kw = mysqli_fetch_assoc($result_text_keywords)) {
-                    $keywords[] = $row_kw['keyword'];
-                }
-
-                $keywords_string = implode(", ", $keywords);
-
-
-                // Retrieving all comments for this text (shows name of member and their comment)
-                $comments = array();
-                $comment_ids = array();
-
-                $sql_text_comments = "SELECT comment_id, member_id, content 
-                                    FROM comment 
-                                    WHERE text_id = $text_id";
-                $result_text_comments = mysqli_query($conn, $sql_text_comments);
-
-                while ($row_comments = mysqli_fetch_assoc($result_text_comments)) {
-
-                    $comment = $row_comments['content'];
-
-                    // Get member name
-                    $sql_member_name = "SELECT name 
-                                        FROM member
-                                        WHERE member_id = " . (int)$row_comments['member_id'];
-                    $result_member_name = mysqli_query($conn, $sql_member_name);
-                    $row_member_name = mysqli_fetch_assoc($result_member_name);
-                    $member_name = $row_member_name['name'];
-
-                    // Save raw text. Escape + add the <br> later
-                    $comments[]    = $comment . "\n\n" . "Posted by [$member_name]";
-                    $comment_ids[] = (int)$row_comments['comment_id'];
-                }
-
-                $keywords_string = implode(", ", $keywords);
-
-                // *** DELETE THE BUTTON DOESN'T DO ANYTHING YET. IT'S JUST THERE  FOR NOW***
-                echo '
-                
-                    <tr>
-                        <td>' . htmlspecialchars($row['title']) . '</td>
-                        <td>' . htmlspecialchars($author_name) . '</td>
-                        <td>' . htmlspecialchars($row['abstract']) . '</td>
-                        <td>' . htmlspecialchars($row['topic']) . '</td>
-                        <td>' . htmlspecialchars($keywords_string) . '</td>
-                        <td>' . htmlspecialchars($row['version']) . '</td>
-                        <td>' . htmlspecialchars($row['upload_date']) . '</td>
-                        <td>' . htmlspecialchars($row['status']) . '</td> 
-                        <td>' . htmlspecialchars($row['download_count']) . '</td> 
-                        <td>' . htmlspecialchars($row['total_donations']) . '</td>
-                        <td>' . htmlspecialchars($row['avg_rating']) . '</td>
-                        
-                        <td>';
-
-                        // Display the comments + reply button (only for the author of this text)
-                        for ($i = 0; $i < count($comments); $i++) {
-
-                            // Show comment text with line breaks
-                            echo nl2br(htmlspecialchars($comments[$i])) . '<br>';
-
-                            // If author is logged in AND they are the author of this text, show Reply button
-                            if (isset($_SESSION['orcid']) &&
-                                isset($row['author_orcid']) &&
-                                $_SESSION['orcid'] === $row['author_orcid']) {
-
-                                echo '
-                                    <form method="get" action="comment_reply.php" style="display:inline;">
-                                        <input type="hidden" name="comment_id" value="' . (int)$comment_ids[$i] . '">
-                                        <input type="hidden" name="text_id" value="' . (int)$text_id . '">
-                                        <button type="submit">Reply</button>
-                                    </form>
-                                ';
-                            }
-
-                            echo '<hr>';
-                        }
-
-                        echo   '</td>
-                        <td>
-                            <form method="post" action="download.php">
-                                <input type="hidden" name="text_id" value="'. $row['text_id'] . '">
-                                <button type="submit" name="download">Download</button>
-                            </form>
-                            <form method="post" action="donate.php">
-                                <input type="hidden" name="text_id" value="'. htmlspecialchars($row['text_id']) . '">
-                                <button type="submit" name="donate">Donate</button>
-                            </form>
-                        ';
-                            
-                    if (!empty($_SESSION['orcid']) && $_SESSION['orcid'] === $row['author_orcid']) {
-                      echo '<form method="post" action="author_item_edit.php">
-                                <input type="hidden" name="text_id" value="'. htmlspecialchars($row['text_id']) . '">
-                                
-                                <input type="hidden" name="title" value="'. htmlspecialchars($row['title']) . '">
-                                <input type="hidden" name="abstract" value="'. htmlspecialchars($row['abstract']) . '">
-                                
-                                <input type="hidden" name="topic" value="'. htmlspecialchars($row['topic']) . '">
-                                
-                                <input type="hidden" name="keywords_string" value="'. htmlspecialchars($keywords_string) . '">
-                                <button type="submit" name="edit">Edit</button>
-                            </form>
-                        ';
-                    }
-
-                        // If the logged-in member is in an active plagiarism committee,
-                        // show a button to open a plagiarism case for this text
-                        if ($is_plag_committee_member && $committee_id_for_user !== null) {
                             echo '
-                            <form method="get" action="plagiarism_case_open.php">
-                                <input type="hidden" name="text_id" value="'. (int)$row['text_id'] . '">
-                                <input type="hidden" name="committee_id" value="'. (int)$committee_id_for_user . '">
-                                <button type="submit" name="open_plagiarism_case">Open Plagiarism Case</button>
-                            </form>
+                                <form method="get" action="comment_reply.php" style="display:inline;">
+                                    <input type="hidden" name="comment_id" value="' . (int)$comment_ids[$i] . '">
+                                    <input type="hidden" name="text_id" value="' . (int)$text_id . '">
+                                    <button type="submit">Reply</button>
+                                </form>
                             ';
                         }
 
+                        echo '<hr>';
+                    }
+
+                    echo   '</td>
+                    <td>
+                        <form method="post" action="download.php">
+                            <input type="hidden" name="text_id" value="'. $row['text_id'] . '">
+                            <button type="submit" name="download">Download</button>
+                        </form>
+                        <form method="post" action="donate.php">
+                            <input type="hidden" name="text_id" value="'. htmlspecialchars($row['text_id']) . '">
+                            <button type="submit" name="donate">Donate</button>
+                        </form>
+                    ';
+                        
+                if (!empty($_SESSION['orcid']) && $_SESSION['orcid'] === $row['author_orcid']) {
+                  echo '<form method="post" action="author_item_edit.php">
+                            <input type="hidden" name="text_id" value="'. htmlspecialchars($row['text_id']) . '">
+                            
+                            <input type="hidden" name="title" value="'. htmlspecialchars($row['title']) . '">
+                            <input type="hidden" name="abstract" value="'. htmlspecialchars($row['abstract']) . '">
+                            
+                            <input type="hidden" name="topic" value="'. htmlspecialchars($row['topic']) . '">
+                            
+                            <input type="hidden" name="keywords_string" value="'. htmlspecialchars($keywords_string) . '">
+                            <button type="submit" name="edit">Edit</button>
+                        </form>
+                    ';
+                }
+
+                    // If the logged-in member is in an active plagiarism committee,
+                    // show a button to open a plagiarism case for this text
+                    if ($is_plag_committee_member && $committee_id_for_user !== null) {
                         echo '
-                        </td> 
-                    </tr>
-                ';
-            }
-            echo '</table>'; // List of texts table closer tag
+                        <form method="get" action="plagiarism_case_open.php">
+                            <input type="hidden" name="text_id" value="'. (int)$row['text_id'] . '">
+                            <input type="hidden" name="committee_id" value="'. (int)$committee_id_for_user . '">
+                            <button type="submit" name="open_plagiarism_case">Open Plagiarism Case</button>
+                        </form>
+                        ';
+                    }
+
+                    echo '
+                    </td> 
+                </tr>
+            ';
         }
+        echo '</table>'; // List of texts table closer tag
     }
-} else {
-    header("Location: login.php");
-    exit;
 }
 
 ?>
